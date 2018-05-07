7e477acb90594c05a6b31b413cce4e7a19952ae1
Liu Yue <hangxingliu@gmail.com>
2018-05-07 08:56:50 +0800

    add example

--- example/index.js
+++ example/index.js
@@ -1,22 +1,6 @@
-# verb-uri
+//@ts-check
 
-[![Build Status](https://travis-ci.org/hangxingliu/node-verb-uri.svg?branch=master)](https://travis-ci.org/hangxingliu/node-verb-uri)
-
-A verb URI parse and stringify Node.js module
-
-## Install
-
-``` bash
-npm install verb-uri
-```
-
-## Example
-
-[example/index.js](example/index.js)
-
-``` javascript
-
-const verbURI = require('verb-uri');
+const verbURI = require('..');
 
 console.log(verbURI.parse('POST /api/book')); // { verb: 'POST', uri: '/api/book' }
 console.log(verbURI.parse('/api/books')); // { verb: 'GET', uri: '/api/books' }
@@ -25,14 +9,3 @@ console.log(verbURI.parse('/api/books', { defaultVerb: 'POST' })); // { verb: 'P
 console.log(verbURI.stringify('post', '/api/book')); // 'POST /api/book'
 console.log(verbURI.stringify('post', '/api/book', { keepCase: true })); // 'post /api/book'
 console.log(verbURI.stringify(null, '/api/books')); // 'GET /api/book'
-
-```
-
-
-## Author
-
-[Liu Yue](https://github.com/hangxingliu)
-
-## License
-
-[GPL-3.0](LICENSE)
